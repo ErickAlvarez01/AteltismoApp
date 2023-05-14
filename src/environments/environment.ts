c3fdb3f3c78ea6d574b97fc897ac6f6822348024
Erick <ericksegarra119@gmail.com>
2023-05-14 13:41:11 -0500

    segunda pagina

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCH19K3W7tK1ZyzDliTrytoVAwhHmY7K4c",
+    authDomain: "atletismoapp-66450.firebaseapp.com",
+    projectId: "atletismoapp-66450",
+    storageBucket: "atletismoapp-66450.appspot.com",
+    messagingSenderId: "431520842414",
+    appId: "1:431520842414:web:b6ddd0fb9b90255b5fa33f"
+  }
 };
 
 /*
